68fa4eba6a9161d89d170de0a2edf27d46a55b80
lijas <lijason991@gmail.com>
2025-11-03 22:53:12 -0500

    added comments

--- src/main/java/com/pluralsight/FixedList.java
+++ src/main/java/com/pluralsight/FixedList.java
@@ -3,24 +3,32 @@ package com.pluralsight;
 import java.util.ArrayList;
 import java.util.List;
 
+// class is generic and accepts any type T of Objects
 public class FixedList<T> {
 
-    private int maxSize;
-    private List<T> items;
+    // private fields, one limiting size of the other
+    private final int maxSize;
+    private final List<T> items;
 
+    // constructor taking in only maxSize, and then initializes new empty ArrayList
     public FixedList(int maxSize) {
         this.maxSize = maxSize;
         this.items = new ArrayList<T>();
     }
 
+    // method to add any type item T to FixedList
     public void add(T item) {
+        // since add can only accept one parameter at a time, check if the FixedList is at maxSize
         if (items.size() == maxSize) {
+            // if yes, throw newly defined IllegalArgumentException
             throw new IllegalArgumentException("Cannot add item: FixedList is already full.");
         } else {
+            // otherwise, add the item to the ArrayList
             items.add(item);
         }
     }
 
+    // getter for ArrayList
     public List<T> getItems() {
         return items;
     }
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // test for attempting to put more than allotted number of integers to FixedList
         FixedList<Integer> numbers = new FixedList<>(3);
         numbers.add(10);
         numbers.add(3);
@@ -15,6 +16,8 @@ public class Main {
         } catch (IllegalArgumentException e) {
             System.out.println("List is full and cannot accept more Objects.");
         }
+
+        // test for attempting to place different Object type into FixedList
         System.out.println(numbers.getItems().size());
         FixedList<LocalDate> dates = new FixedList<>(2);
         dates.add(LocalDate.now());
